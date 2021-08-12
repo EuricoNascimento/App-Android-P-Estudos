ae9edfc0a7e2e4e645e0efc1654ed5038b22cd39
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-08-12 18:56:52 -0300

    ciração de algumas classes e consumo da API

--- app/src/main/java/com/example/elricoapp/presentation/details/BookDetailsActivity.kt
+++ app/src/main/java/com/example/elricoapp/presentation/details/BookDetailsActivity.kt
@@ -1,4 +1,24 @@
 package com.example.elricoapp.presentation.details
 
-class BookDetailsActivity {
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.os.Bundle
+import com.example.elricoapp.presentation.books.BooksViewModel
+import androidx.lifecycle.Observer
+import com.example.elricoapp.R
+
+class BookDetailsActivity : AppCompatActivity() {
+    private lateinit var viewModel: BooksViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_books)
+
+        //toolbarMain2.apply {
+
+
+    }
 }
