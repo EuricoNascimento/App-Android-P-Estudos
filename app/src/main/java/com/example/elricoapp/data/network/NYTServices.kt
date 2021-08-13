54765423d74d2c42a68bf104d9f83130067408e4
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-08-13 10:13:33 -0300

    vai dar certo

--- app/src/main/java/com/example/elricoapp/data/network/NYTServices.kt
+++ app/src/main/java/com/example/elricoapp/data/network/NYTServices.kt
@@ -1,4 +1,17 @@
 package com.example.elricoapp.data.network
 
+import com.example.elricoapp.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 interface NYTServices {
+    @GET("lists.json")
+    fun getBooks(
+        @Query("list") List: String = "hardcover-fiction",
+        @Query("api-key") apikey: String = "joKE3A6jJS3w7cXDnVCBjM3Sc0S0SqsJA"
+    ) : Call<BookBodyResponse>
+
+
 }
