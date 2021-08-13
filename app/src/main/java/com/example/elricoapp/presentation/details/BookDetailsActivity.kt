9abe558f3a2a68a535b5a5b1ea61d7063b15cc8c
João Lucas <64292055+garbageMonkey@users.noreply.github.com>
2021-08-13 15:22:21 -0300

    Update BookDetailsActivity.kt

--- app/src/main/java/com/example/elricoapp/presentation/details/BookDetailsActivity.kt
+++ app/src/main/java/com/example/elricoapp/presentation/details/BookDetailsActivity.kt
@@ -1,28 +1,5 @@
 package com.example.elricoapp.presentation.details
 
-import android.content.Context
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import android.os.Bundle
-import com.example.elricoapp.presentation.books.BooksViewModel
-import androidx.lifecycle.Observer
-import com.example.elricoapp.R
-
-class BookDetailsActivity : AppCompatActivity() {
-    private lateinit var viewModel: BooksViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_books)
-
-        //toolbarMain2.apply {
-
-
-    }
-=======
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -55,8 +32,6 @@ class BookDetailsActivity {
                 putExtra(EXTRA_DESCRIPTION, description)
             }
         }
-        
     }
-
->>>>>>> feature/003
+    
 }
