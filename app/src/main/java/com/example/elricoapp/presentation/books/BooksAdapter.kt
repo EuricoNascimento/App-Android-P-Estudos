eca0a559d4180acbb090563f6953b79385bf6db4
Eurico Neto <euriconascimentofn@gmail.com>
2021-08-13 12:04:44 -0300

    Criado os arquivos 123

--- app/src/main/java/com/example/elricoapp/presentation/books/BooksAdapter.kt
+++ app/src/main/java/com/example/elricoapp/presentation/books/BooksAdapter.kt
@@ -1,5 +1,40 @@
 package com.example.elricoapp.presentation.books
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.elricoapp.R
+import com.example.elricoapp.data.model.Book
 
-class BooksAdapter {
+class BooksAdapter (
+    private val books: List<Book>,
+    val onItemClickListener: ((book: Book) -> Unit)
+): RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_books, parent, false)
+        return BooksViewHolder(view, onItemClickListener)
+    }
+
+    override fun onBindViewHolder(Viewholder: BooksViewHolder, position: Int) {
+        val book = books[position]
+        ViewHolder.bindView(book)
+    }
+
+    override fun getItemCount() = books.count()
+
+    class BooksViewHolder(itemView: View, val onItemClickListener: (book: Book) -> Unit) :
+        RecyclerView.ViewHolder(itemView) {
+        private val title = itemView.tv_title
+        private val author = itemView.tv_autor
+
+        fun bindView(book: Book) {
+            title.text = book.title
+            author.text = book.author
+
+            itemView.setOnClickListener {
+                onItemClickListener.invoke(book)
+            }
+        }
+    }
 }
